19156c126d08cb64c1b297f605a7672fce087c52
JhonnyV-V <jhonny_varela_visbal@hotmail.com>
2024-07-03 15:27:52 -0400

    codecrafters submit [skip ci]

--- app/server.go
+++ app/server.go
@@ -63,21 +63,7 @@ func userAgent(headers []string) []byte {
 	return okWithBody(agent, "text/plain")
 }
 
-func main() {
-	fmt.Println("Logs from your program will appear here!")
-
-	l, err := net.Listen("tcp", "0.0.0.0:4221")
-	if err != nil {
-		fmt.Println("Failed to bind to port 4221")
-		os.Exit(1)
-	}
-
-	connection, err := l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
-	}
-
+func HandleConnection(connection net.Conn)  {
 	buff := bufio.NewReader(connection)
 	raw, err := buff.ReadString('\n')
 	if err != nil {
@@ -102,4 +88,23 @@ func main() {
 		fmt.Println(err)
 	}
 	connection.Close()
+}
+
+func main() {
+	fmt.Println("Logs from your program will appear here!")
+
+	l, err := net.Listen("tcp", "0.0.0.0:4221")
+	if err != nil {
+		fmt.Println("Failed to bind to port 4221")
+		os.Exit(1)
+	}
+	for {
+		connection, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			os.Exit(1)
+		}
+
+		go HandleConnection(connection)
+	}
 }
