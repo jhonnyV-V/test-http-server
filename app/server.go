814f38406abdb182002dce6847ad75ed3d4765a2
JhonnyV-V <jhonny_varela_visbal@hotmail.com>
2024-07-03 16:45:24 -0400

    codecrafters submit [skip ci]

--- app/server.go
+++ app/server.go
@@ -6,12 +6,16 @@ import (
 	"fmt"
 	"net"
 	"os"
+	"strconv"
 	"strings"
 )
 
 const (
+	GET       = "GET"
+	POST      = "POST"
 	OK        = "HTTP/1.1 200 OK\r\n\r\n"
 	NOT_FOUND = "HTTP/1.1 404 Not Found\r\n\r\n"
+	CREATED   = "HTTP/1.1 201 Created\r\n\r\n"
 )
 
 var FileDir string
@@ -48,6 +52,23 @@ func parseHeaders(buff *bufio.Reader) []string {
 	return headers
 }
 
+func parseBody(buff *bufio.Reader, headers []string) string {
+	var body []byte
+	var size int
+
+	for _, v := range headers {
+		if strings.HasPrefix(v, "Content-Length") {
+			value := strings.TrimSpace(strings.Split(v, ":")[1])
+			size, _ = strconv.Atoi(value)
+		}
+	}
+	body, err := buff.Peek(size)
+	if err != nil {
+		panic(err)
+	}
+	return string(body)
+}
+
 func echo(fullpath string) []byte {
 	path := strings.Split(fullpath, "/")
 	message := path[2]
@@ -72,13 +93,22 @@ func getFiles(reqPath string) []byte {
 
 	raw, err := os.ReadFile(FileDir + fileName)
 	if err != nil {
-		fmt.Println("error: ", err)
-		fmt.Println("fileName: ", fileName)
-		fmt.Println("fileDir: ", FileDir)
 		return []byte(NOT_FOUND)
 	}
 
 	return okWithBody(string(raw), "application/octet-stream")
+}
+
+func uploadFile(reqPath string, body string) []byte {
+	path := strings.Split(reqPath, "/")
+	fileName := path[2]
+
+	err := os.WriteFile(FileDir+fileName, []byte(body), 0666)
+	if err != nil {
+		return []byte(NOT_FOUND)
+	}
+
+	return []byte(CREATED)
 }
 
 func HandleConnection(connection net.Conn) {
@@ -91,23 +121,34 @@ func HandleConnection(connection net.Conn) {
 	fmt.Println(raw)
 
 	args := strings.Split(raw, " ")
-	if args[1] == "/" {
-		_, err = connection.Write([]byte(OK))
-	} else if strings.HasPrefix(args[1], "/echo/") {
-		_, err = connection.Write(echo(args[1]))
-	} else if strings.HasPrefix(args[1], "/user-agent") {
-		headers := parseHeaders(buff)
-		response := userAgent(headers)
-		_, err = connection.Write(response)
-	} else if strings.HasPrefix(args[1], "/files") {
-		_, err = connection.Write(getFiles(args[1]))
-	} else {
-		_, err = connection.Write([]byte(NOT_FOUND))
+	if args[0] == GET {
+		if args[1] == "/" {
+			_, err = connection.Write([]byte(OK))
+		} else if strings.HasPrefix(args[1], "/echo/") {
+			_, err = connection.Write(echo(args[1]))
+		} else if strings.HasPrefix(args[1], "/user-agent") {
+			headers := parseHeaders(buff)
+			response := userAgent(headers)
+			_, err = connection.Write(response)
+		} else if strings.HasPrefix(args[1], "/files") {
+			_, err = connection.Write(getFiles(args[1]))
+		} else {
+			_, err = connection.Write([]byte(NOT_FOUND))
+		}
+		return
 	}
-
-	if err != nil {
-		fmt.Println(err)
+	if args[0] == POST {
+		if strings.HasPrefix(args[1], "/files") {
+			headers := parseHeaders(buff)
+			body := parseBody(buff, headers)
+			response := uploadFile(args[1], body)
+			_, err = connection.Write(response)
+		} else {
+			_, err = connection.Write([]byte(NOT_FOUND))
+		}
+		return
 	}
+	_, err = connection.Write([]byte(NOT_FOUND))
 }
 
 func main() {
