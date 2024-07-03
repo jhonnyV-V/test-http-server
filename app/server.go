51a8771ce635a1b22992db2b8f81ab800557e335
JhonnyV-V <jhonny_varela_visbal@hotmail.com>
2024-07-03 14:18:22 -0400

    codecrafters submit [skip ci]

--- app/server.go
+++ app/server.go
@@ -13,6 +13,24 @@ const (
 	NOT_FOUND = "HTTP/1.1 404 Not Found\r\n\r\n"
 )
 
+func okWithBody(body, ctype string) []byte {
+	response := fmt.Sprintf(
+		"HTTP/1.1 200 OK\r\nContent-Type: %s\r\nContent-Length: %d\r\n\r\n%s",
+		ctype,
+		len(body),
+		body,
+	)
+	fmt.Println("response", response)
+	return []byte(response)
+}
+
+func echo(fullpath string) []byte {
+	path := strings.Split(fullpath, "/")
+	message := path[2]
+
+	return okWithBody(message, "text/plain")
+}
+
 func main() {
 	fmt.Println("Logs from your program will appear here!")
 
@@ -33,11 +51,13 @@ func main() {
 	if err != nil {
 		fmt.Println(err)
 	}
-	// fmt.Println(raw)
+	fmt.Println(raw)
 
 	args := strings.Split(raw, " ")
 	if args[1] == "/" {
 		_, err = connection.Write([]byte(OK))
+	} else if strings.HasPrefix(args[1], "/echo/") {
+		_, err = connection.Write(echo(args[1]))
 	} else {
 		_, err = connection.Write([]byte(NOT_FOUND))
 	}
